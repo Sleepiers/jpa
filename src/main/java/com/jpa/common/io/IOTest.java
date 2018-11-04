d4b7c0ecbebd5edec1a1c0a9c252ec0d5d208b41
Sleepiers <1360162808@qq.com>
2018-11-04 22:20:45 +0800

    IO学习

---
+++ src/main/java/com/jpa/common/io/IOTest.java
@@ -0,0 +1,11 @@
+package com.jpa.common.io;
+
+/**
+ * @user zzw
+ * @date 2018/11/04 0004
+ */
+public class IOTest {
+    public static void main(String[] args) {
+        System.out.println("IOTest打印");
+    }
+}
