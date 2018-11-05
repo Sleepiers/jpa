ce5438968b7b8d976639bf4542685a8df8c01492
zhengzw <1360162808@qq.com>
2018-11-05 17:50:20 +0800

    从配置文件中读取bean

--- src/test/java/com/jpa/JpaApplicationTests.java
+++ src/test/java/com/jpa/JpaApplicationTests.java
@@ -2,15 +2,22 @@ package com.jpa;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpaApplicationTests {
 
+    @Autowired
+    ApplicationContext ioc;
+
     @Test
     public void contextLoads() {
+        boolean personService = ioc.containsBean("personService");
+        System.out.println(personService);
     }
 
 }
